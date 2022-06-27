2777a0e63aed1c95e9339c4b1a6fe1750cf03fe6
Fabio-F16 <fipsfabio95@gmail.com>
2022-06-27 14:20:48 +0200

    primo esercizio

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int numeroInserito;
+            int somma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Inserisci un numero");
+                numeroInserito = Convert.ToInt32(Console.ReadLine());
+                somma += numeroInserito;
+            }
+            Console.WriteLine(somma);
         }
     }
 }
