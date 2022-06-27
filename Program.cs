61f2ee809b722f19a96b6677dbc4b7eb45a28163
Fabio-F16 <fipsfabio95@gmail.com>
2022-06-27 14:41:06 +0200

    snack 4

--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // secondo esercizio snack4
+            int[] numeri = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sum = 0;
+            float average;
+            for (int i = 0; i < numeri.Length; i++)
+            {
+                sum += numeri[i];
+                
+            }
+            
+            average = sum / numeri.Length;
+            Console.WriteLine("la somma dei numeri è " + sum);
+            Console.WriteLine("la media dei numeri è " + average);
+
+            // fine secondo esercizio snack4
+
+
+
+
+            return;
+            // primo esercizio snack3
             int numeroInserito;
             int somma = 0;
 
@@ -14,7 +35,8 @@
                 numeroInserito = Convert.ToInt32(Console.ReadLine());
                 somma += numeroInserito;
             }
-            Console.WriteLine(somma);
+            Console.WriteLine("La somma dei numeri inseriti è: " + somma);
+            // fine primo esercizio 
         }
     }
 }
